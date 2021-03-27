548f09bb7038d616b7615915c9245e394fcb8a9b
mykhailo.maidan@gmail.com <Mykhailo>
2021-03-27 12:35:16 +0200

    Move auth to different file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use oauth2_examples::{config_from_args, listen_for_code};
 use reqwest::header::{HeaderName,HeaderValue,ACCEPT,CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 
+pub mod spotify;
+use spotify::autorize::*;
+
 #[derive(Deserialize)]
 #[derive(Debug)]
 struct ExternalUrls
@@ -39,36 +42,12 @@ async fn main() -> anyhow::Result<()> {
 
     let reqwest_client = reqwest::Client::new();
 
-    let auth_url = Url::parse("https://accounts.spotify.com/authorize")?;
-    let token_url = Url::parse("https://accounts.spotify.com/api/token")?;
-    let redirect_url = Url::parse("http://localhost:8080/api/auth/redirect")?;
+    let token = spotify::autorize::get_auto_token(config.client_id, config.client_secret).await.unwrap();
 
-    let mut client = Client::new(config.client_id, auth_url, token_url);
-    client.set_client_secret(config.client_secret);
-    client.add_scope("user-read-email");
-    client.set_redirect_url(redirect_url);
-
-    let state = State::new_random();
-    let auth_url = client.authorize_url(&state);
-
-    println!("Browse to: {}", auth_url);
-
-    let received = listen_for_code(8080).await?;
-
-    if received.state != state {
-        panic!("CSRF token mismatch :(");
-    }
-
-    let token = client
-        .exchange_code(received.code)
-        .with_client(&reqwest_client)
-        .execute::<StandardToken>()
-        .await?;
-
-    println!("Token: {:?}", token.access_token().to_string());
+    println!("Token: {}", token.to_string());
 
     let response = reqwest_client.get("https://api.spotify.com/v1/me")
-    .bearer_auth(token.access_token().to_string())
+    .bearer_auth(token.to_string())
     .header(ACCEPT,HeaderValue::from_bytes(b"application/json").unwrap())
     .header(CONTENT_TYPE,HeaderValue::from_bytes(b"application/json").unwrap())
     .send()
---
+++ src/spotify/autorize.rs
@@ -0,0 +1,45 @@
+use oauth2::{Client, Token, StandardToken, State, Url};
+use oauth2_examples::{config_from_args, listen_for_code};
+use reqwest::header::{HeaderName,HeaderValue,ACCEPT,CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+const AUTH_URL : &str = "https://accounts.spotify.com/authorize";
+const TOKEN_URL : &str = "https://accounts.spotify.com/api/token";
+const REDIRECT_URL : &str = "http://localhost:8080/api/auth/redirect";
+
+pub struct SpotifyToken(String);
+
+impl fmt::Display for SpotifyToken {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+pub async fn get_auto_token(client_id: String, client_secret: String) -> Result<SpotifyToken, String> 
+{
+    let received_client = reqwest::Client::new();
+    let auth_url = Url::parse(AUTH_URL).unwrap();
+    let token_url = Url::parse(TOKEN_URL).unwrap();
+    let redirect_url = Url::parse(REDIRECT_URL).unwrap();
+    let mut client = Client::new(client_id, auth_url, token_url);
+    client.set_client_secret(client_secret);
+    client.add_scope("user-read-email");
+    client.set_redirect_url(redirect_url);
+
+    let state = State::new_random();
+    let auth_url = client.authorize_url(&state);
+
+    println!("Browse to: {}", auth_url);
+
+    let received = listen_for_code(8080).await.unwrap();
+
+    let token = client.exchange_code(received.code)
+                      .with_client(&received_client)
+                      .execute::<StandardToken>()
+                      .await.unwrap();
+
+    Ok(SpotifyToken(token.access_token().to_string()))                 
+}
+
+
