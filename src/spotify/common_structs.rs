aee73bde7bab4988f828385ad465b55d0eaf1284
MYMAIDAN <mykhailo.maidan@gmail.com>
2021-06-20 20:53:41 +0300

    Add Image struct to common

--- src/spotify/common_structs.rs
+++ src/spotify/common_structs.rs
@@ -4,3 +4,11 @@ pub enum ResponseValue<T>
     value(T),
     error(Error),
 }
+
+#[derive(Deserialize,Debug)]
+pub struct Images 
+{
+    height : u32,
+    url : Option<String>,
+    width : u32
+}
--- src/spotify/user_top_artist_track.rs
+++ src/spotify/user_top_artist_track.rs
@@ -1,43 +1,45 @@
-use super::user_profile::*;
 use serde::{Deserialize, Serialize};
-#[derive(Deserialize, Debug)]
+use super::user_profile::*;
+
+
+#[derive(Deserialize)]
+#[derive(Debug)]
 pub struct ExternalUrls
 {
-    spotify: String,
+    spotify : String,
 }
-#[derive(Deserialize, Debug)]
+
+#[derive(Deserialize)]
+#[derive(Debug)]
 pub struct Followers
 {
-    href: Option<String>,
-    total: u32,
+    href : Option<String>,
+    total: u32
 }
-#[derive(Deserialize, Debug)]
-pub struct Images
-{
-    height: u32,
-    url: Option<String>,
-    width: u32,
-}
-#[derive(Deserialize, Debug)]
+
+#[derive(Deserialize)]
+#[derive(Debug)]
 pub struct Items
 {
-    external_urls: ExternalUrls,
-    followers: Followers,
-    genres: Vec<String>,
-    href: Option<String>,
-    id: String,
-    images: Vec<Images>,
-    name: String,
-    popularity: u32,
-    uri: Option<String>,
+    external_urls : ExternalUrls,
+    followers : Followers,
+    genres : Vec<String>,
+    href : Option<String>,
+    id : String,
+    images : Vec<Images>,
+    name : String,
+    popularity : u32,
+    uri : Option<String>
 }
-#[derive(Deserialize, Debug)]
+
+#[derive(Deserialize)]
+#[derive(Debug)]
 pub struct UserTopArtistAndTraks
 {
-    items: Vec<Items>,
-    next: Option<String>,
-    previous: Option<String>,
-    total: u32,
-    limit: u32,
-    href: Option<String>,
+    items : Vec<Items>,
+    next : Option<String>,
+    previous : Option<String>,
+    total : u32,
+    limit : u32,
+    href : Option<String>
 }
