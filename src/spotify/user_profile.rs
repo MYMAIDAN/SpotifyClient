038061af6d5154ff307f2faf2e71fae891a4d2da
MYMAIDAN <mykhailo.maidan@gmail.com>
2021-07-06 17:35:35 +0300

    Refactore user profile structs

--- src/spotify/user_profile.rs
+++ src/spotify/user_profile.rs
@@ -5,33 +5,33 @@ use super::common_structs::*;
 #[derive(Deserialize, Debug)]
 struct Followers
 {
-    href: Option<String>,
-    total: u8,
+    href  : Option<String>,
+    total : u8,
 }
 #[derive(Deserialize, Debug)]
 pub struct CurrentUserProfile
 {
-    display_name: String,
-    email: String,
-    external_urls: ExternalUrls,
-    followers: Followers,
-    href: Option<String>,
-    id: String,
-    images: Vec<Option<String>>,
-    r#type: Option<String>,
-    uri: String,
+    display_name  : String,
+    email         : String,
+    external_urls : ExternalUrls,
+    followers     : Followers,
+    href          : Option<String>,
+    id            : String,
+    images        : Vec<Option<String>>,
+    r#type        : Option<String>,
+    uri           : String,
 }
 #[derive(Deserialize, Debug)]
 pub struct UserProfile
 {
-    display_name: String,
-    external_urls: ExternalUrls,
-    followers: Followers,
-    href: Option<String>,
-    id: String,
-    images: Vec<Option<String>>,
-    r#type: Option<String>,
-    uri: String,
+    display_name  : String,
+    external_urls : ExternalUrls,
+    followers     : Followers,
+    href          : Option<String>,
+    id            : String,
+    images        : Vec<Option<String>>,
+    r#type        : Option<String>,
+    uri           : String,
 }
 
 
