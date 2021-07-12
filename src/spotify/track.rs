020640ee38151f21b2d9b91f9b55173680a243c8
MYMAIDAN <mykhailo.maidan@gmail.com>
2021-07-12 20:33:53 +0300

    Add doc to track struct

--- src/spotify/track.rs
+++ src/spotify/track.rs
@@ -1,6 +1,10 @@
 use serde::{Deserialize, Serialize};
 use super::common_structs::*;
 
+
+/// Struct for track api https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-tracks
+/// This structure for part of API where using market attribute is not empty
+/// - available_markest is allowd when market attribute not empty
 #[derive(Deserialize, Debug)]
 pub struct LocalTrackAlbum
 {
@@ -20,6 +24,9 @@ pub struct LocalTrackAlbum
 
 }
 
+/// Struct for track api https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-tracks
+/// This structure for part of API where  market attribute default or empty
+/// - available_markest is allowd when market attribute empty
 #[derive(Deserialize, Debug)]
 pub struct GlobalTrackAlbum
 {
@@ -36,9 +43,9 @@ pub struct GlobalTrackAlbum
     total_tracks           : u32,
     r#type                 : String,
     uri                    : Option<String>,
-
 }
 
+/// Struct for link resource from outside
 #[derive(Deserialize, Debug)]
 pub struct LinkedFrom 
 {
@@ -49,6 +56,10 @@ pub struct LinkedFrom
     uri           : Option<String>
 }
 
+/// Struct for track api https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-tracks
+/// This structure for part of API where  market attribute not empty
+/// - is local is not allowd when market attribute  empty
+/// - is playable is not allowd when market attribute  empty
 #[derive(Deserialize, Debug)]
 pub struct GlobalTrack
 {
@@ -72,6 +83,10 @@ pub struct GlobalTrack
     uri           : Option<String>
 }
 
+/// Struct for track api https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-tracks
+/// This structure for part of API where  market attribute not empty
+/// - is local is allowd when market attribute not empty
+/// - is playable is allowd when market attribute not empty
 #[derive(Deserialize, Debug)]
 pub struct LocalTrack
 {
@@ -93,11 +108,4 @@ pub struct LocalTrack
     track_number  : u32,
     r#type        : String,
     uri           : Option<String>
-}
-
-#[derive(Deserialize, Debug)]
-pub enum Track
-{
-    eGlobal(GlobalTrack),
-    eLocal(LocalTrack),
 }
